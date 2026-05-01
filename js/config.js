02b1d66306380a8de1ccba54213bb84acae4d6ff
Luis David <a01800657@tec.mx>
2026-05-01 08:57:10 -0600

    Conexión Cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_mSr6hJUmL', 
+        userPoolClientId: '3bifi0jd0n8v9ptencodsf3pp9', 
+        region: 'us-east-1' 
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
